5811b1a8728f41af13d6e878a7e3f7a612b410a4
Roman Gontar <ashenoooone@gmail.com>
2024-05-18 16:03:09 +0400

    feat тест на настройки клиента

--- testing/tests/routes/RouterOptionClientTest.py
+++ testing/tests/routes/RouterOptionClientTest.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 
 from fastapi.testclient import TestClient
+from loguru import logger
 
 from src.api.main import app
 
@@ -10,40 +11,48 @@ headers = {
 }
 
 
-async def test_get_option_client():
+def test_get_option_client():
     # валидное получение настреок юзера
     client = TestClient(app, base_url='http://testserver/options_clients', headers=headers)
-    # todo ломается из-за тайзоны
     response = client.get("123451234")
+    asserted_response = {'id': 1, 'id_client': 2, 'is_notification': False, 'timezone': 0}
     assert response.status_code == 200
+    assert json.loads(response.text) == asserted_response
+    logger.success("ТЕСТ НА ВАЛИДНОЕ ПОЛУЧЕНИЕ НАСТРОЕК УСПЕШНО ВЫПОЛНЕН")
 
     # не найдены настроки
     response = client.get('1')
     assert response.status_code == 500
-    assert response.text == '"ошибка при поиске"'
+    logger.success("ТЕСТ НА ОШИБКУ ПРИ ПОЛУЧЕНИИ НАСТРОЕК УСПЕШНО ВЫПОЛНЕН")
 
 
-async def test_put_option_client():
+def test_put_option_client():
     # валидный кейс изменения настроек
     client = TestClient(app, base_url='http://testserver/options_clients', headers=headers)
-    payload = {"id": "5", "id_client": "6", "is_notification": False, "timezone": 0}
+    payload = {"id": 1, "id_client": 2, "is_notification": False, "timezone": 0}
     response = client.put("/", json=payload)
-    result = {"id": 5, "id_client": 6, "is_notification": False, "timezone": 0}
+    result = {"id": 1, "id_client": 2, "is_notification": False, "timezone": 0}
     assert response.status_code == 200
     assert result == json.loads(response.text)
+    logger.success("ТЕСТ НА ВАЛИДНОЕ ИЗМЕНЕНИЕ НАСТРОЕК УСПЕШНО ВЫПОЛНЕН")
 
     # клиент не найден
-    payload = {"id": 6, "id_client": 4, "is_notification": False, "timezone": 0}
+    payload = {"id": 1, "id_client": 4, "is_notification": False, "timezone": 0}
     response = client.put("/", json=payload)
-    assert response.text == '"ошибка при поиске"'
     assert response.status_code == 500
+    logger.success("ТЕСТ НА ОШИБКУ ПРИ ОБНОВЛЕНИИ НАСТРОЕК ( КЛИЕНТ НЕ НАЙДЕН ) УСПЕШНО ВЫПОЛНЕН")
 
     # настройки не найдены
-    payload = {"id": 6, "id_client": 6, "is_notification": False, "timezone": 0}
+    payload = {"id": 3, "id_client": 1, "is_notification": False, "timezone": 0}
     response = client.put("/", json=payload)
-    assert response.text == '"ошибка при поиске"'
     assert response.status_code == 500
+    logger.success("ТЕСТ НА ОШИБКУ ПРИ ОБНОВЛЕНИ НАСТРОЕК ( НАСТРОЙКИ НЕ НАЙДЕНЫ ) УСПЕШНО ВЫПОЛНЕН")
+
+
+def main():
+    # test_get_option_client()
+    test_put_option_client()
 
 
 if __name__ == '__main__':
-    asyncio.run(test_get_option_client())
+    main()
